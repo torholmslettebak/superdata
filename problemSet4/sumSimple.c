0ac1b257a5085cde2f93ecc6487df7dd925df866
torholmslettebak <torholmslettebak@gmail.com>
2015-02-05 13:11:24 +0100

    Added folder for problem set 4, and added sumSimple.c which so far only takes vector size from argument.

---
+++ problemSet4/sumSimple.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	// argv[1] is size of Vector v
+	int n = atoi(argv[1]);
+	printf("The size of generated vector: %d\n", n);
+	return 0;
+}

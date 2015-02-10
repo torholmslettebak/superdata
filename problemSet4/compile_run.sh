75378485c69c5b5b4cc16b73309441bc61e145b2
torholmslettebak <torholmslettebak@gmail.com>
2015-02-10 22:34:29 +0100

    Loads of changes, but code more clean and functioning

--- problemSet4/compile_run.sh
+++ problemSet4/compile_run.sh
@@ -1,8 +1,20 @@
-
-
-gcc sumSimple.c -o vectorsum  -lm
+source /opt/intel/bin/iccvars.sh intel64
+source /opt/intel/bin/compilervars.sh intel64
+#gcc sumSimple.c -o vectorsum  -lm
 # possible input: 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-./vectorsum 3 4 5 6 7 8 9 10 11 12 13 14 15
+#./vectorsum 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 
 
-gcc -o vectorsumomp -fopenmp sumOmp.c -lm -O3
-OMP_NUM_THREADS=3 ./vectorsumomp 3 4 5 6 7 8 9 10 11 12 13 14 15
+#icc sumSimple.c -o vectorsum  -lm -02
+#./vectorsum 6
+
+icc -O3 -o vectorsumomp -fopenmp sumOmp.c -lm
+export OMP_NUM_THREADS=1
+./vectorsumomp 14
+
+#icc -o vectorsumomp -fopenmp sumOmp.c -lm -O2
+export OMP_NUM_THREADS=2
+./vectorsumomp 14
+
+#icc -o vectorsumomp -fopenmp sumOmp.c -lm -O2
+export OMP_NUM_THREADS=4
+./vectorsumomp 14
--- problemSet4/sumOmp.c
+++ problemSet4/sumOmp.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#ifdef _OPENMP
+#include <omp.h>
+/*#ifdef _OPENMP
     #include <omp.h>
 #else
     #define omp_get_num_threads() 0
     #define omp_get_thread_num() 0
 #endif
+*/
 
 void printVector(int size, double *vector)
 {
+	printf("Here comes the vector\n");
 	int i;
 	for (i = 0; i < size; i++)
 	{
@@ -18,46 +21,59 @@ void printVector(int size, double *vector)
 	}
 	printf("\n");
 }
+double myf(double x)
+{
+  return 1.0/(x*x*1.0);
+}
 
+double WallTime ()
+{
+#ifdef HAVE_MPI
+  return MPI_Wtime();
+#elif defined(HAVE_OPENMP)
+  return omp_get_wtime();
+#else
+  struct timeval tmpTime;
+  gettimeofday(&tmpTime,NULL);
+  return tmpTime.tv_sec + tmpTime.tv_usec/1.0e6;
+#endif
+}
 
 double* generateVector(int size, double *vector)
 {
-	int counter = 0;
 	int i;
 	vector = malloc(size*sizeof(double));
-	for (i = 1; i < size+1; i++)
+	#pragma omp parallel \
+	shared (size) \
+	private (i)
+	#pragma omp for schedule(static) 
+	for (i = 0; i < size; i++)
 	{
-		//printf("%lf\n", 1 / (i*i));
-		vector[counter] = 1 / (double)(i*i);
-		counter++;
+		#pragma omp critical //
+		vector[i] = (double) (1 / (i+1)*(i+1));
 	}
 	return vector;
 }
 
+double* generateVector2(int size, double *vector)
+{
+	int counter = 0;
+	int i, thread_id;
+	vector = malloc(size*sizeof(double));
+
+	return vector;
+}
 
 
 double computeSum(int size, double *vector)
 {
-	double sum, testsum;
 	int i;
-
-	//int num_threads, thread_id;
-//#pragma omp parallel private(i, testsum)
-//	{	
-//		#pragma omp for reduction(+ : sum) schedule(static)
-		for (i = 0; i < size; i++)
-		{
-			//num_threads = omp_get_num_threads();
-			//thread_id = omp_get_thread_num();
-			//printf("Hello. I am thread %d out of a team of %d\n", thread_id, num_threads);
-			testsum = vector[i];
-			sum += testsum;
-			//printf("Vector[i]: %lf\n", vector[i]);
-			//printf("the sum so far: %lf\n", sum);
-			//printf("The sum so far: %lf\n", sum);
-		}
-	//printf("the sum after loop: %lf\n", sum);
-//	}
+	double sum;
+	#pragma omp parallel for reduction(+:sum) schedule(static)
+	for (i = 0; i < size; i++)
+	{
+		sum += vector[i];
+	}
 	return sum;
 }
 double computeDifference(double sum)
@@ -74,45 +90,34 @@ int main(int argc, char *argv[])
 	printf ( "  Number of threads =              %d\n", omp_get_max_threads ( ) );
 	int counter = 1;
 	int n, i;
-	double *vector;
-	clock_t start, end;
-	double cpu_time_used;
-	start = clock();
+	double *vector, t1, t2, dt;
+	t1 = WallTime();
 	int num_threads, thread_id;
-	//int threads = omp_get_threads();
-	//printf("total num of threads is: %d\n", threads);
 	if (argc == 0)
 	{
 		printf("Error no vector size has been given in commandline \n");
 		exit(0);
 	}
-	#pragma omp parallel for schedule(dynamic) private(num_threads, thread_id)
+	/*Går gjenom argv og kjører metoder ut fra argv*/
 	for (i = 1; i < argc; i++)
 	{
-		num_threads = omp_get_num_threads();
-		thread_id = omp_get_thread_num();
-		printf("Hello. I am thread %d out of a team of %d\n", thread_id, num_threads);
 
 		n = pow(2.0, atoi(argv[i]));
-		//printf("The size of generated vector: %d\n", n);
 		// The vector is v(i) = 1/i^2
 		// following generates a vector
 		double actualSum = M_PI * M_PI / 6;
 		vector = generateVector(n, vector);
-		//printVector(n, vector);
 		double sum = computeSum(n, vector);
 		
 
-		/*printf("the sum of all vector elements: %lf\n", sum);
+		printf("the sum of all vector elements: %lf\n", sum);
 		printf("the actual sum as vector size goes -> inf: %lf\n", actualSum);
 		printf("The difference for, %d elements is: %lf\n", n, computeDifference(sum));
-		*/
-		//printf("summing %d vector elements took: %lf seconds\n", n, cpu_time_used );
-		//printf("\n");
+
 	}
-	end = clock();
-	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("the program, with generating and summing (WTIH OMP) took: %lf seconds\n", cpu_time_used );
+	t2 = WallTime();
+  	dt = t2 - t1;
+	printf("the program, with generating and summing (WTIH OMP) took: %lf seconds\n", dt );
 
 	return 0;
 }
